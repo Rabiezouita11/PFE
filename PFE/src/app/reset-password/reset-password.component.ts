012cd1afc417bc9484b66b82460fa18981f8b837
rabiezouita11 <rabiezouita82@gmail.com>
2024-04-24 11:54:12 +0100

    modifier tempalte reset password and forget password

--- PFE/src/app/reset-password/reset-password.component.ts
+++ PFE/src/app/reset-password/reset-password.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/fo
 import { UserService } from '../_services/user.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MustMatch } from '../validator/must-match.validator';
+import Swal from 'sweetalert2';
+import { ScriptStyleLoaderService } from '../Service/ScriptStyleLoaderService/script-style-loader-service.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -16,11 +18,13 @@ export class ResetPasswordComponent implements OnInit {
   successMessage: string | null = null;
   errorMessage: string | null = null;
 
-  constructor(public service: UserService, public fb: FormBuilder,
+  constructor(private scriptStyleLoaderService: ScriptStyleLoaderService ,public service: UserService, public fb: FormBuilder,
     private router: Router, private route: ActivatedRoute,
   ) { }
 
   ngOnInit() {
+    this.loadScriptsAndStyles();
+
     this.infoForm();
     this.route.queryParams.subscribe(params => {
       this.token = params['token'];
@@ -32,7 +36,37 @@ export class ResetPasswordComponent implements OnInit {
   get f(): { [key: string]: AbstractControl } {
     return this.form.controls;
   }
+  loadScriptsAndStyles(): void {
+    const SCRIPT_PATH_LIST = [
+      'assets/frontoffice/vendors/js/vendor.bundle.base.js',
+      'assets/frontoffice/vendors/chart.js/Chart.min.js',
+      'assets/frontoffice/vendors/datatables.net/jquery.dataTables.js',
+      'assets/frontoffice/vendors/datatables.net-bs4/dataTables.bootstrap4.js',
+      'assets/frontoffice/js/dataTables.select.min.js',
+      'assets/frontoffice/js/off-canvas.js',
+      'assets/frontoffice/js/hoverable-collapse.js',
+      'assets/frontoffice/js/template.js',
+      'assets/frontoffice/js/settings.js',
+      'assets/frontoffice/js/todolist.js',
+      'assets/frontoffice/js/dashboard.js',
+      'assets/frontoffice/js/Chart.roundedBarCharts.js',
+      'https://code.jquery.com/jquery-3.6.0.min.js'
+    ];
+    const STYLE_PATH_LIST = [
+      'assets/frontoffice/vendors/feather/feather.css',
+      'assets/frontoffice/vendors/ti-icons/css/themify-icons.css',
+      'assets/frontoffice/vendors/css/vendor.bundle.base.css',
+      'assets/frontoffice/vendors/datatables.net-bs4/dataTables.bootstrap4.css',
+      'assets/frontoffice/vendors/ti-icons/css/themify-icons.css',
+      'assets/frontoffice/js/select.dataTables.min.css',
+      'assets/frontoffice/css/vertical-layout-light/style.css',
+      'assets/frontoffice/images/favicon.png'
+    ];
+    this.scriptStyleLoaderService.loadScripts(SCRIPT_PATH_LIST),
+      this.scriptStyleLoaderService.loadStyles(STYLE_PATH_LIST)
+    // Show the loader
 
+  }
   infoForm() {
     this.form = this.fb.group({
       password: ['', [Validators.required, Validators.minLength(8)]],
@@ -52,15 +86,26 @@ export class ResetPasswordComponent implements OnInit {
     this.submitted = true;
     this.successMessage = '';
     this.errorMessage = '';
+    
     this.service.resetPassword(this.token, this.form.value.password)
-      .subscribe((response: any) => {
-        this.successMessage = response.message;
-        this.router.navigate(['/login']);
-
-      },
-      (error) => {
-        this.errorMessage = error.error.message;
-      }
-    );
+      .subscribe(
+        (response: any) => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: response.message
+          }).then(() => {
+            this.router.navigate(['/login']);
+          });
+        },
+        (error) => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: error.error.message
+          });
+        }
+      );
   }
+  
 }
